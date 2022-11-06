a56edf090a129ce03644835ee1daa2f93df0c573
PeteChicchetti <pj0524@gmail.com>
2022-11-06 16:41:02 -0500

    added DELETE request to category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -62,8 +62,24 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteCategory = await Category.destroy ({
+      where: {
+        id:req.params.id,
+      },
+    });
+    // If statement for a null user
+    if (!deleteCategory) {
+      res.status(404).json({ message: 'No category found with that ID!'});
+      return;
+    }
+    // Respond with json from the api
+    res.status(200).json(deleteCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
